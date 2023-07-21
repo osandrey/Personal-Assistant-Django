46ddcfa5b8e0686403e8ed419240d193a1010538
KarynaSam <ondatra410@gmail.com>
2023-07-21 22:24:24 +0300

    Created models

--- src/notesapp/models.py
+++ src/notesapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from usersapp.models import User
+
+
+class Tag(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+
+
+class Note(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    tags = models.ManyToManyField(Tag)
+
--- src/usersapp/models.py
+++ src/usersapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.validators import UnicodeUsernameValidator
 
-# Create your models here.
+
+class User(models.Model):
+    email = models.EmailField(blank=True)
+    username_validator = UnicodeUsernameValidator()
+    username = models.CharField(
+        max_length=150,
+        unique=True,
+        help_text="Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.",
+        validators=[username_validator]
+    )
+    password = models.CharField(max_length=128)
+    created_at = models.DateTimeField(blank=True, default=None)
+    avatar = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)

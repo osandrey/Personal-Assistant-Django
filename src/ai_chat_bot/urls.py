e7f2513db91c152ef889c1c269e573b16f5576ed
osandrey <osandreyman@gmail.com>
2023-07-23 17:31:38 +0100

    Added

--- src/ai_chat_bot/urls.py
+++ src/ai_chat_bot/urls.py
@@ -8,5 +8,7 @@ app_name = 'ai_chat_bot'
 
 urlpatterns = [
     path('', main, name='main'),
-    path('/tg', redirect_check, name='redirect_check')
+    # path('tg/<str:query>/', redirect_check, name='redirect_check')
+    path('redirect_check/(?P<query>[^/]+)?/', redirect_check, name='redirect_check'),
+
     ]
